551fe95ec33c61bc6b43ac4f4e2acd388fb7d9ea
zq0618 <71154579+zq0618@users.noreply.github.com>
2021-08-30 14:05:24 -0500

    Sherry

---
+++ src/Heartbeat.java
@@ -0,0 +1,22 @@
+
+public class Heartbeat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double time = 0;
+		
+		while (time < 10) 
+		{
+			try {
+				Thread.sleep(100);
+				time += 0.1;
+				System.out.println("time elapsed: " + time);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+				
+			}
+		}
+
+	}
+
+}
